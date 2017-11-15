015d88d60f1b15992bcb5d1160c99013a4f00820
veikherm <agatetx@gmail.com>
2017-11-15 11:27:17 +0200

    wxPython

--- ec2_connect.sh
+++ ec2_connect.sh
@@ -3,6 +3,9 @@ export EC2_SERVER="ubuntu@ec2-34-215-198-232.us-west-2.compute.amazonaws.com"
 ssh -i "deepami.pem" $EC2_SERVER
 sshfs -o IdentityFile=~/deepami.pem $EC2_SERVER:/home/ubuntu ~/remote
 ssh -i "deepami.pem" -L 6006:localhost:6006 $EC2_SERVER
+scp  -r -i "../deepami.pem" $EC2_SERVER:/home/ubuntu/disk2/datasets/sd* .
+rsync -arsync -azvv -e "ssh -i deepami.pem" $EC2_SERVER:/home/ubuntu/disk2/datasets/sd*
+rsync -azvv --progress dev-dsk-veikherm-2b-8fd2062d.us-west-2.amazon.com:/home/veikherm/s3local /media/veikherm/DT_Black/ORVILLE_DATA
 
 
 
