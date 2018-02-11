c8530ebcf4c3fd88f69fcfc685d7e23ac0deeff6
veikherm <agatetx@gmail.com>
2018-02-11 13:59:34 +0200

    newest nvidia driver

--- install_cuda.py
+++ install_cuda.py
@@ -10,6 +10,12 @@ os.system("wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cud
 os.system("dpkg -i cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64-deb")
 
 if 0:
+    # Cudnn 7
+    os.system('wget https://www.dropbox.com/s/a6jo871mclhb9e3/libcudnn7_7.0.5.15-1%2Bcuda9.1_amd64.deb -O cudnn-7.0.deb')
+    os.system('sudo dpkg -i  cudnn-7.0.deb')
+    os.system('wget https://www.dropbox.com/s/oyz8qnsgy7vhxhx/libcudnn7-dev_7.0.5.15-1%2Bcuda9.1_amd64.deb -O cudnn-dev-7.0.deb')
+    os.system('sudo dpkg -i  cudnn-dev-7.0.deb')
+
     # Cudnn 6
     os.system('wget https://www.dropbox.com/s/w2211kd0u0vgtgs/cudnn-8.0-linux-x64-v6.0.tgz?dl=0 -O cudnn.tgz')
     os.system('rm -rf /opt/cudnn/')
@@ -23,6 +29,7 @@ if 0:
     os.system('tar -xvzf cudnn-5.1.tgz -C /opt/cudnn-5.1/')
 
 
+
     os.system('''echo 'export LD_LIBRARY_PATH=/opt/cudnn/cuda/lib64:$LD_LIBRARY_PATH' >>~/.bashrc''')
     os.system('timedatectl set-timezone Asia/Jerusalem')
     os.system('updatedb &> /dev/null')
@@ -30,4 +37,6 @@ if 0:
 
 # Handle nvidia version HELL
 os.system('apt -y remove nvidia-3*')
-os.system('apt -y --allow-unauthenticated install nvidia-375-dev')
+os.system('add-apt-repository -y ppa:graphics-drivers')
+os.system('apt-get update')
+os.system('apt -y --allow-unauthenticated install nvidia-390-dev')
