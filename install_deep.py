4d1863ad960bd11781209a3eb43fa595cf92abd9
Leifman <gleifman@amazon.com>
2018-05-16 12:42:56 +0300

    pytorch 0.4.0

--- install_deep.py
+++ install_deep.py
@@ -8,13 +8,13 @@ args = parser.parse_args()
 
 
 if args.use_cuda:
-    os.system('sudo pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl')
-    os.system('sudo pip install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp27-cp27mu-linux_x86_64.whl') 
+    os.system('sudo pip3 install pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp35-cp35m-linux_x86_64.whl')
+    os.system('sudo pip install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl') 
     os.system('sudo pip3 install tensorflow-gpu')
     os.system('sudo pip install tensorflow-gpu')
 else:
-    os.system('sudo pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl ')
-    os.system('sudo pip install http://download.pytorch.org/whl/cpu/torch-0.3.0.post4-cp27-cp27mu-linux_x86_64.whl ') 
+    os.system('sudo pip3 install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp35-cp35m-linux_x86_64.whl ')
+    os.system('sudo pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl ') 
     os.system('sudo pip3 install tensorflow')
     os.system('sudo pip install tensorflow')
  
